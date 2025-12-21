5c1594df77f60b2d1d574f272221634940490601
Turbo170 <ar235023@gmail.com>
2025-12-21 16:38:21 -0500

    week 1: variable and output

---
+++ week1/variables.py
@@ -0,0 +1,12 @@
+name = "Alejandro Rodriguez"
+age = 21
+favoriteGame = "Roblox"
+
+txt = f"My name is {name}."
+print(txt)
+
+txt = f"I am {age} years old."
+print(txt)
+
+txt = f"My favorite game is {favoriteGame}."
+print(txt)
