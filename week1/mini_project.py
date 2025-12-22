7a0229ef9fb36d214681328869d9b4d890d1758c
Turbo170 <ar235023@gmail.com>
2025-12-21 20:05:19 -0500

    week 1:Mini-Project-OPM text game

---
+++ week1/mini_project.py
@@ -0,0 +1,8 @@
+print ("Enter Name")
+name = input()
+print(f"Welcome {name}, to the one punch man game.")
+option = int(input("You are walking and encounter a monster,\ndo you:\n1 Stand your ground and fight\n2 Walk past the enemy"))
+if (option) == 1.:
+    print(f"You throw a punch, instantly killing it.\nYou Win!")
+elif option == 2.:
+    print(f"You leave the monster confused and they continue with their rampage.\n You Lose.")
