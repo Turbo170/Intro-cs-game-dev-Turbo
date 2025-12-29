52aefe960c54a9c2d5448ef1f6abf7ea1aa37bb4
Turbo170 <ar235023@gmail.com>
2025-12-29 17:18:11 -0500

    Week 1 Adjustments to Assignment 2 - Inputs_conditionals.py

--- week1/input_conditionals.py
+++ week1/input_conditionals.py
@@ -1,7 +1,10 @@
+#user inputs a name and age which both become variables for the if statement
 print("What is your name?")
 name = input()
 age = int(input("How old are you?"))
 
+"""if statment is used with age variable to print different sentences with name variable.
+ elif is if else, used to give different sentences if something is other than, etc, etc"""
 if (age) < 13:
     print(f"Hello {name}, you are a Child.")
 elif age <= 18:
