567528a61c3f23d85d411d2ab46f599015cda9c3
Turbo170 <ar235023@gmail.com>
2025-12-29 00:12:09 -0500

    Week 2 Mini Assignment 1 - Countdown Loop

--- week2/countdown.py
+++ week2/countdown.py
@@ -1,5 +1,9 @@
+#variable of I equals to 10
 i = 10
+
+#while I is greater or equal to 1, it shall print the variable I counting down from 10 to 1
 while i >=1:
     print(i)
     i -= 1
 print ("Blast off!")
+#after printing I fully, prints statement blast off showing the code has ended
