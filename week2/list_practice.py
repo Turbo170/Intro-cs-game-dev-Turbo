66afee9bb1f57fb35729434fcb2c719bdc991a3f
Turbo170 <ar235023@gmail.com>
2025-12-29 00:08:13 -0500

    Week 2 Mini Assignment 2 - List Practice

---
+++ week2/list_practice.py
@@ -0,0 +1,10 @@
+#items is the variable that equals the list of words"
+items = ["senzu bean", "dragon stone", "chrono crystal", "support character", "equipment"]
+
+#for the specific item in the list of items, it will print every "item"
+for item in items:
+    print(item)
+else:
+    print(len(items))
+
+#using else after the items are all printed to present the number of items in the list
