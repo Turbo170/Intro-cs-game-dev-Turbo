be98accecaeac304b197d9c3470ebdf4b35aa6f1
Turbo170 <ar235023@gmail.com>
2025-12-30 00:53:34 -0500

    Week2 Main Assignment - Battle sim (Partially finished)

---
+++ week2/battle_simulator.py
@@ -0,0 +1,15 @@
+import random
+import time
+
+#this is the health of both player and enemy
+PlayerHealth = 100
+EnemyHealth = 100
+
+Attacks = {"Throw a punch, throw a right hook, feint into a kick"}
+
+print("The battle begins...")
+
+while PlayerHealth > 0 and EnemyHealth > 0:
+    input("\nPress Enter to attack")
+
+    break
