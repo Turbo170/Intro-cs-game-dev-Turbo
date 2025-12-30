cee9ce3bda79c8fe2ac5a65b6613c5f7381cf509
Turbo170 <ar235023@gmail.com>
2025-12-29 23:08:43 -0500

    Week 2 Mini Assignment 3 - random_Damage.py

--- week2/random_damage.py
+++ week2/random_damage.py
@@ -2,3 +2,4 @@ import random
 
 AttackDamage = random.randint(1, 32)
 print(f"Your attack has done {AttackDamage} damage.")
+#Made the variable use the imported random, combining it with randint so it gives me a random number between 1-32. Then i print the chosen number in a sentence revealing it.
