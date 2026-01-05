85705b1e95d7ca1b4906e17f6fbe9862621e085b
Turbo170 <ar235023@gmail.com>
2026-01-05 18:49:23 -0500

    Week 3: Mini Assignment 3 - Importing from another file

---
+++ import_practice.py
@@ -0,0 +1,5 @@
+from utilities import greet_player, get_starting_hp
+
+greet_player()
+Hp = get_starting_hp()
+print("Your Hp:", Hp)
---
+++ utilities.py
@@ -0,0 +1,5 @@
+def greet_player():
+    print("Hello Player, Welcome to my game.")
+
+def get_starting_hp():
+    return 30
