969ad96a4762e685f497892849160921f85817db
Turbo170 <ar235023@gmail.com>
2026-01-05 21:01:50 -0500

    Week 3: Comments on import_practice and utilities.py

--- import_practice.py
+++ import_practice.py
@@ -3,3 +3,5 @@ from utilities import greet_player, get_starting_hp
 greet_player()
 Hp = get_starting_hp()
 print("Your Hp:", Hp)
+#this was simple practice for importing code from one file to another
+#This is the commit file
--- utilities.py
+++ utilities.py
@@ -3,3 +3,5 @@ def greet_player():
 
 def get_starting_hp():
     return 30
+#this is practice for importing code from one file to another
+#this is the functions
